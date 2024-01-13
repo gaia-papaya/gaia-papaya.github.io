c9d5b4fe4a989acfd8a75c2a95007b78f925b514
gaia-papaya <car9715@nyu.edu>
2024-01-12 22:05:29 -0500

    where do i even begin... i think im starting to like this web dev shit.. its not as hard as doing stats all day, although the battles are over much simpler things, so the toil doesnt feel as worth it. oh well

--- movingparts.js
+++ movingparts.js
@@ -1,4 +1,4 @@
-//tab flipper
+//tab flipper (also flips header images)
 function TabFlipper(event, TabName) {
     //get all elements with class of 'tabcontent' and hide
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -6,12 +6,30 @@ function TabFlipper(event, TabName) {
     tabcontent[i].style.display = "none";
 }
 
-    //get all elementswith class of 'tablink' and remove class of 'active'
+//get all elements of class 'topimage' and hide
+tabimage = document.getElementsByClassName("topimage");
+for(i = 0; i <tabimage.length; i++) {
+    tabimage[i].style.display = "none"
+}
+
+    //get all elements with class of 'tablink' and remove class of 'active'
     tablinks = document.getElementsByClassName("tablink")
     for(i = 0; i < tablinks.length; i++){
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+
     // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(TabName).style.display = "block";
   event.currentTarget.className += " active";
+
+  TabImageName = TabName + "ImageLeft"
+  document.getElementById(TabImageName).style.display = "block"
+  document.getElementById(TabImageName).style.transition = .33;
+
+  TabImageName1 = TabName + "ImageRight"
+  document.getElementById(TabImageName1).style.display = "block";
+  //
+
+
 }
+
