1488dbcfbe74f4ad92d551b5d1106d83f8961920
gaia-papaya <car9715@nyu.edu>
2024-01-15 15:01:09 -0500

    trying out a new, single page layout giving me eadhaches, so giving up for a bit :b

--- movingparts.js
+++ movingparts.js
@@ -6,30 +6,19 @@ function TabFlipper(event, TabName) {
     tabcontent[i].style.display = "none";
 }
 
-//get all elements of class 'topimage' and hide
-tabimage = document.getElementsByClassName("topimage");
-for(i = 0; i <tabimage.length; i++) {
-    tabimage[i].style.display = "none"
-}
-
     //get all elements with class of 'tablink' and remove class of 'active'
     tablinks = document.getElementsByClassName("tablink")
     for(i = 0; i < tablinks.length; i++){
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
+// Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(TabName).style.display = "block";
   event.currentTarget.className += " active";
 
-  TabImageName = TabName + "ImageLeft"
-  document.getElementById(TabImageName).style.display = "block"
-  document.getElementById(TabImageName).style.transition = .33;
-
-  TabImageName1 = TabName + "ImageRight"
-  document.getElementById(TabImageName1).style.display = "block";
-  //
-
-
 }
+
+document.getElementsByClassName("tablink active")
+
+
 
