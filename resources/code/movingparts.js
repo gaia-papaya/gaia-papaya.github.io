8cdac296ecd98a7e00ea06407b76dafb6d352ccc
gaia-papaya <car9715@nyu.edu>
2024-08-21 14:23:03 -0400

    smoke weed erry day

--- resources/code/movingparts.js
+++ resources/code/movingparts.js
@@ -43,7 +43,7 @@ window.onscroll = function() {
     if(CurrentScrollPos < PreviousScrollPos) {
         document.getElementById("topbar").style.top = "0";
     } else {
-        document.getElementById("topbar").style.top = "-5rem";
+        document.getElementById("topbar").style.top = "-150px";
     }
     PreviousScrollPos = CurrentScrollPos;
 }
